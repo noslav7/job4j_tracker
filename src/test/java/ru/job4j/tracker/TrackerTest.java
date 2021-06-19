6541e5eed7fc72619d8c8d1d8fab78c57f93e6c0
noslav7 <noslav7@gmail.com>
2021-06-19 21:40:58 +0500

    2. Репозиторий под проект Tracker [#245623 #169181]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,10 +1,12 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
+
     @Test
     public void whenAddNewItemThenTrackerHasSameItem() {
         Tracker tracker = new Tracker();
