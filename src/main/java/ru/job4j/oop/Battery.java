d1c8164d4d66c6f5b92b987dbf2aa4b57d9f2c8a
noslav7 <noslav7@gmail.com>
2021-06-21 14:35:06 +0500

    1.8. Состояние объекта [#167388]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -3,13 +3,13 @@ package ru.job4j.oop;
 public class Battery {
     private int load;
 
+    public Battery(int charge) {
+        this.load = charge;
+    }
+
     public void exchange(Battery another) {
         another.load = another.load + this.load;
         this.load = 0;
-    }
-
-    public Battery(int charge) {
-        this.load = charge;
     }
 
     public static void main(String[] args) {
