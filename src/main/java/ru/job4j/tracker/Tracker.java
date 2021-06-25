99c25e06cc164b5e22ab6587e05e1a3fe1cd010e
noslav7 <noslav7@gmail.com>
2021-06-25 20:56:54 +0500

    5. Tracker - хранилище [#396 #171106]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,18 +1,21 @@
 package ru.job4j.tracker;
+
 import java.util.Arrays;
 
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
+    private int size = 0;
 
     public Item add(Item item) {
         item.setId(ids++);
+        items[size++] = item;
         return item;
     }
 
     public Item findById(int id) {
         Item rsl = null;
-        for (int index = 0; index < items.length - 1; index++) {
+        for (int index = 0; index < size; index++) {
             Item item = items[index];
             if (item.getId() == id) {
                 rsl = item;
@@ -22,20 +25,31 @@ public class Tracker {
         return rsl;
     }
 
-    public Item[] findByName(int siz) {
+    public Item[] findByName(String key) {
         Item[] namesWithoutNull = new Item[items.length];
-        for (int i  = 0; i < siz; i++) {
-            String name = items[i].getName();
-            if (name.equals(siz)) {
-                namesWithoutNull[siz] = items[i];
-                siz++;
+        int size = 0;
+        for (Item item : items) {
+            String name = item.getName();
+            if (name.equals(key)) {
+                namesWithoutNull[size] = item;
+                size++;
             }
         }
-        namesWithoutNull = Arrays.copyOf(namesWithoutNull, siz);
+        namesWithoutNull = Arrays.copyOf(namesWithoutNull, size);
         return namesWithoutNull;
     }
 
     public Item[] findAll() {
-        return Arrays.copyOf(items, items.length);
+        Item[] namesWithoutNull = new Item[][items.length];
+        int size = 0;
+        for (Item name : items) {
+            String cell = name.getName();
+            if (cell != null) {
+                namesWithoutNull[size] = name;
+                size++;
+            }
+        }
+        namesWithoutNull = Arrays.copyOf(namesWithoutNull, size);
+        return namesWithoutNull;
     }
 }
