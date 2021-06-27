952d772a7425625d352991b8eb8c42552bf0697b
noslav7 <noslav7@gmail.com>
2021-06-27 23:07:18 +0500

    7. Метод удаления заявки Tracker.delete [#211749 #171539]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -62,7 +62,7 @@ public class  Tracker {
         boolean result = index != -1;
         if (result) {
             items[index] = null;
-            System.arraycopy(items, index + 1, index, index, size - index - 1);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             size--;
         }
         return result;
