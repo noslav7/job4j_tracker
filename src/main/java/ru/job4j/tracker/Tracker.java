1a14d5051e42bd60594a2e9cc2e56dece9f2ccf2
noslav7 <noslav7@gmail.com>
2021-06-28 10:47:41 +0500

    2.1. Реализация класса StartUI [#784]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,7 +1,7 @@
 package ru.job4j.tracker;
 import java.util.Arrays;
 
-public class Tracker {
+public class  Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
