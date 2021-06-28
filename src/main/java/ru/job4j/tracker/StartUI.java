d9abe90d3c3b15a8ecee723a2a6a7b81c29e9bf3
noslav7 <noslav7@gmail.com>
2021-06-28 10:44:56 +0500

    2.1. Реализация класса StartUI [#784]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,8 +1,93 @@
 package ru.job4j.tracker;
 
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class StartUI {
-    public static void main(String[] args) {
-        Item item = new Item(5, "Шариковая ручка");
-        System.out.println(item);
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            this.showMenu();
+            System.out.println("Select:");
+            int select = Integer.valueOf(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Create a new Item ====");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+            } else if (select == 1) {
+                System.out.println("=== Show all Items ====");
+            Item[] items = tracker.findAll();
+            for (Item item: items) {
+                System.out.println(item);
+            }
+            } else if (select == 2) {
+                System.out.println("=== Edit Item ====");
+                String requestId = scanner.nextLine();
+                int id = Integer.parseInt(requestId);
+                String req = scanner.nextLine();
+                Item request = new Item(req);
+                tracker.replace(id, request);
+                if (tracker.replace(id, request)) {
+                    System.out.println("Замена выполнена");
+                } else {
+                    System.out.println("Ошибка при попытке заменить");
+                }
+            } else if (select == 3) {
+                System.out.println("=== Delete Item ====");
+                String requestId = scanner.nextLine();
+                int id = Integer.parseInt(requestId);
+                tracker.delete(id);
+                if (tracker.delete(id)) {
+                    System.out.println("Удаление выполнено");
+                } else {
+                    System.out.println("Ошибка при попытке удалить");
+                }
+            } else if (select == 4) {
+                System.out.println("=== Find Item by Id ====");
+                String requestId = scanner.nextLine();
+                int id = Integer.parseInt(requestId);
+                Item item = tracker.findById(id);
+                if (item != null) {
+                    System.out.println(item);
+                } else {
+                    System.out.println("Заявка с таким id не найдена");
+                }
+            } else if (select == 5) {
+                System.out.println("Find Items by Name");
+                String req = scanner.nextLine();
+                Item[] items = tracker.findByName(req);
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Заявки с таким именем не найдены");
+                }
+            } else if (select == 6) {
+                System.out.println("Exit Program");
+                run = false;
+            }
+        }
     }
-}
+
+    private void showMenu() {
+        System.out.println("Menu.");
+        System.out.println("0. Add new Item");
+        System.out.println("1. Show all items");
+        System.out.println("2. Edit item");
+        System.out.println("3. Delete item");
+        System.out.println("4. Find item by Id");
+        System.out.println("5. Find items by name");
+        System.out.println("6. Exit Program");
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
+
+    }
+ }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -4,38 +4,67 @@ import java.util.Arrays;
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
+    private int size = 0;
 
     public Item add(Item item) {
         item.setId(ids++);
+        items[size++] = item;
         return item;
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < items.length - 1; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] namesWithoutNull = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < this.size; index++) {
+            String name = items[index].getName();
+            if (name.equals(key)) {
+                namesWithoutNull[size] = items[index];
+                size++;
+            }
+        }
+        namesWithoutNull = Arrays.copyOf(namesWithoutNull, size);
+        return namesWithoutNull;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
     }
 
-    public Item[] findByName(int siz) {
-        Item[] namesWithoutNull = new Item[items.length];
-        for (int i  = 0; i < siz; i++) {
-            String name = items[i].getName();
-            if (name.equals(siz)) {
-                namesWithoutNull[siz] = items[i];
-                siz++;
-            }
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            item.setId(id);
+            items[index] = item;
         }
-        namesWithoutNull = Arrays.copyOf(namesWithoutNull, siz);
-        return namesWithoutNull;
+        return rsl;
     }
 
-    public Item[] findAll() {
-        return Arrays.copyOf(items, items.length);
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        boolean result = index != -1;
+        if (result) {
+            items[index] = null;
+            System.arraycopy(items, index + 1, index, index, size - index - 1);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            size--;
+        }
+        return result;
     }
 }
