e86532b11a8dac0f4bb6f3d59f3381afaccad95d
noslav7 <noslav7@gmail.com>
2021-06-29 18:57:33 +0500

    2.6. Реализация класса StartUI. Удаление заявки [#500747]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -60,8 +60,9 @@ public class  Tracker {
         int index = indexOf(id);
         boolean result = index != -1;
         if (result) {
-            System.arraycopy(items, index + 1, index, index, size - index - 1);
-            items[size-1] = null;
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
         }
         return result;
     }
