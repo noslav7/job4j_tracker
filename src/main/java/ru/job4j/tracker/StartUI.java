fcb824d25e1e6a9a0f5fb7c3bddf2afc48f8dff5
noslav7 <noslav7@gmail.com>
2021-06-29 08:45:30 +0500

    2.1. Реализация класса StartUI [#784]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,7 +3,7 @@ package ru.job4j.tracker;
 import java.lang.reflect.Array;
 import java.util.Scanner;
 
-public class  StartUI {
+public class StartUI {
     public void init(Scanner scanner, Tracker tracker) {
         boolean run = true;
         while (run) {
