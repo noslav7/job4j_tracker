8696e8f87d828b2adb868394169d117c61c888b5
noslav7 <noslav7@gmail.com>
2021-06-29 11:26:11 +0500

    3.1. Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ src/main/java/ru/job4j/oop/Colledge.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Colledge {
+    public static void main(String[] args) {
+        Freshman freshman = new Freshman();
+        Student student = freshman;
+        Object object = freshman;
+    }
+}
---
+++ src/main/java/ru/job4j/oop/HierarchyUsage.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+public class HierarchyUsage {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+
+        Transport tp = car;
+
+        Object obj = car;
+
+        Object ocar = new Car();
+
+        Car carFromObject = (Car) car;
+
+        Object bycicle = new Bicycle();
+        Car cb = (Car) bycicle;
+    }
+}
