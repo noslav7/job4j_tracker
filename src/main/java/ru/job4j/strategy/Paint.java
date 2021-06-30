8043958f23ef1aae8564c663ed3e4cf90e4a6c02
noslav7 <noslav7@gmail.com>
2021-06-30 22:43:45 +0500

    7. Шаблон проектирования - Стратегия [#786 #173104]

--- src/main/java/ru/job4j/strategy/Paint.java
+++ src/main/java/ru/job4j/strategy/Paint.java
@@ -1,6 +1,6 @@
 package ru.job4j.strategy;
 
-public class Paint {
+public class  Paint {
     public void draw(Shape shape) {
         System.out.println(shape.draw());
     }
--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -5,9 +5,9 @@ public class Square implements Shape {
     @Override
     public String draw() {
         String ln = System.lineSeparator();
-            return " ^ " + ln
-               + " /   \\" + ln
-               + "/_____\\" + ln;
+            return "|¯¯¯¯¯¯|" + ln
+                 + "|      |" + ln
+                 + "|______|" + ln;
         }
 
         public static void main (String[]args){
