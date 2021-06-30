2550f8ee8767a777e623a7357b36f06037f0a840
noslav7 <noslav7@gmail.com>
2021-06-30 08:34:34 +0500

    4.1. Разрыв зависимости StartUI от Scanner. [#181778]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Array;
 import java.util.Scanner;
 
 
-public class StartUI {
+public class  StartUI {
 
     public void init(Input input, Tracker tracker) {
         boolean run = true;
