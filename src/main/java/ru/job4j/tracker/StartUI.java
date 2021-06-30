754b5272098119aa0e6351b19fc861511afda76a
noslav7 <noslav7@gmail.com>
2021-06-30 09:12:06 +0500

    4.2. Статические методы. [#181779]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,52 +5,69 @@ import java.util.Scanner;
 
 
 public class  StartUI {
+    private static String msg = "Enter Id";
+
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Create a New Item ====");
+        System.out.println("Enter name: ");
+        String msg = "Enter Id: ";
+        String name = input.askStr(msg);
+        Item item = new Item(name);
+        tracker.add(item);
+        System.out.println("Добавленная заявка" + item);
+    }
+
+    public static void showItems(Input input, Tracker tracker) {
+        System.out.println("=== Show all items ====");
+        Item[] items = tracker.findAll();
+        if (items.length > 0) {
+            for (Item item : items) {
+                System.out.println(item);
+            }
+        } else {
+            System.out.println("Хранилище еще не содержит заявок");
+        }
+    }
+
+    public static void editItem(Input input, Tracker tracker) {
+        System.out.println("=== Edit item ====");
+        System.out.println("Enter Id: ");
+        int id = Integer.parseInt(input.askStr(msg));
+        System.out.println("Enter name: ");
+        String name = input.askStr(msg);
+        Item item = new Item(name);
+        if (tracker.replace(id, item)) {
+            System.out.println("Заявка заменена успешно.");
+        } else {
+            System.out.println("Ошибка замены заявки.");
+        }
+    }
+
+    public static void enterId(Input input, Tracker tracker) {
+        System.out.println("=== Delete item ====");
+        System.out.println("Enter Id: ");
+        int id = Integer.parseInt(input.askStr(msg));
+        if (tracker.delete(id)) {
+            System.out.println("Заявка удалена успешно.");
+        } else {
+            System.out.println("Ошибка удаления заявки.");
+        }
+    }
 
     public void init(Input input, Tracker tracker) {
         boolean run = true;
-        String msg = "Enter Id: ";
         while (run) {
             showMenu();
             System.out.println("Select: ");
             int select = Integer.parseInt(input.askStr(msg));
             if (select == 0) {
-                System.out.println("=== Create a New Item ====");
-                System.out.println("Enter name: ");
-                String name = input.askStr(msg);
-                Item item = new Item(name);
-                tracker.add(item);
-                System.out.println("Добавленная заявка" + item);
+                StartUI.createItem(input, tracker);
             } else if (select == 1) {
-                System.out.println("=== Show all items ====");
-                Item[] items = tracker.findAll();
-                if (items.length > 0) {
-                    for (Item item : items) {
-                        System.out.println(item);
-                    }
-                } else {
-                    System.out.println("Хранилище еще не содержит заявок");
-                }
+                StartUI.showItems(input, tracker);
             } else if (select == 2) {
-                System.out.println("=== Edit item ====");
-                System.out.println("Enter Id: ");
-                int id = Integer.parseInt(input.askStr(msg));
-                System.out.println("Enter name: ");
-                String name = input.askStr(msg);
-                Item item = new Item(name);
-                if (tracker.replace(id, item)) {
-                    System.out.println("Заявка заменена успешно.");
-                } else {
-                    System.out.println("Ошибка замены заявки.");
-                }
+                StartUI.editItem(input, tracker);
             } else if (select == 3) {
-                System.out.println("=== Delete item ====");
-                System.out.println("Enter Id: ");
-                int id = Integer.parseInt(input.askStr(msg));
-                if (tracker.delete(id)) {
-                    System.out.println("Заявка удалена успешно.");
-                } else {
-                    System.out.println("Ошибка удаления заявки.");
-                }
+                StartUI.enterId(input, tracker);
             } else if (select == 6) {
                 run = false;
             }
