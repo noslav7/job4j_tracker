d2475c8d5ff22ddecbaf19e378dba0f48e14f50f
noslav7 <noslav7@gmail.com>
2021-07-02 09:15:06 +0500

    9.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,89 +5,10 @@ import java.util.Scanner;
 
 
 public class StartUI {
+        private final Output out;
 
-
-    public static void createItem(Input input, Tracker tracker) {
-        System.out.println("=== Create a New Item ====");
-        System.out.println("Enter name: ");
-        String msg = "Enter Id: ";
-        String name = input.askStr("Enter name: ");
-        Item item = new Item(name);
-        tracker.add(item);
-        System.out.println("Добавленная заявка" + item);
-    }
-
-    public static void showItems(Input input, Tracker tracker) {
-        System.out.println("=== Show all items ====");
-        Item[] items = tracker.findAll();
-        String name = input.askStr("Enter name: ");
-        if (items.length > 0) {
-            for (Item item : items) {
-                System.out.println(item);
-            }
-        } else {
-            System.out.println("Хранилище еще не содержит заявок");
-        }
-    }
-
-    public static void editItem(Input input, Tracker tracker) {
-        System.out.println("=== Edit item ====");
-        System.out.println("Enter Id: ");
-        int id = input.askInt("Enter Id: ");
-        System.out.println("Enter name: ");
-        String name = input.askStr("Enter name: ");
-        Item item = new Item(name);
-        if (tracker.replace(id, item)) {
-            System.out.println("Заявка заменена успешно.");
-        } else {
-            System.out.println("Ошибка замены заявки.");
-        }
-    }
-
-    public static void deleteItem(Input input, Tracker tracker) {
-        System.out.println("=== Delete item ====");
-        System.out.println("Enter Id: ");
-        int id = Integer.parseInt(input.askStr("Enter name: "));
-        if (tracker.delete(id)) {
-            System.out.println("Заявка удалена успешно.");
-        } else {
-            System.out.println("Ошибка удаления заявки.");
-        }
-    }
-
-    public static void findById(Input input, Tracker tracker) {
-        System.out.println("=== Find item by id ====");
-        System.out.print("Enter id: ");
-        int id = Integer.parseInt(input.askStr("Enter name: "));
-        Item item = tracker.findById(id);
-        if (item != null) {
-            System.out.println(item);
-        } else {
-            System.out.println("Заявка с введенным id: " + id + " не найдена.");
-        }
-    }
-
-    public static void findByName(Input input, Tracker tracker) {
-        System.out.println("=== Find items by name ====");
-        System.out.print("Enter name: ");
-        String name = input.askStr("Enter name: ");
-        Item[] items = tracker.findByName(name);
-        if (items.length > 0) {
-            for (Item item : items) {
-                System.out.println(item);
-            }
-        } else {
-            System.out.println("Заявки с именем: " + name + " не найдены.");
-        }
-    }
-
-    public static void replaceItem(Input input, Tracker tracker) {
-        System.out.println(" === Update item ====");
-        int id = input.askInt("Enter id:");
-        String name = input.askStr("Enter a new name of item: ");
-        Item item = new Item(name);
-        item.setId(id);
-        tracker.replace(id, item);
+        public StartUI(Output out) {
+            this.out = out;
     }
 
     public void init(Input input, Tracker tracker, UserAction[] actions) {
@@ -108,11 +29,17 @@ public class StartUI {
     }
 
     public static void main (String[]args){
+        Output output = new ConsoleOutput();
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction()
+                new CreateAction(output),
+                new Exit(),
+                new DeleteAction(),
+                new EditAction(),
+                new FindIdAction(),
+                new ShowAction()
         };
-        new StartUI().init(input, tracker, actions);
+        new StartUI(output).init(input, tracker, actions);
     }
  }
