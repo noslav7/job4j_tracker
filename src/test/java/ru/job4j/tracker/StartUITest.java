760e135e4147ee0a3c797c09ed54234fe6326127
noslav7 <noslav7@gmail.com>
2021-07-02 10:23:51 +0500

    9. Написать тесты на StartUI. [#785 #173411]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -8,20 +8,47 @@ import static org.junit.Assert.assertThat;
 public class  StartUITest {
 
     @Test
-    public void whenExit() {
-        Output out = new StubOutput();
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Replaced item"));
+        String replacedName = "New item name";
         Input in = new StubInput(
-                new String[] {"0"}
+                new String[] {"0", "id", "Edited item", "1"}
+        );
+        UserAction[] actions = {
+                new EditAction(),
+                new Exit()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Deleted item"));
+        Input in = new StubInput(
+                new String[] {"0", "id", "1"}
+        );
+        UserAction[] actions = {
+                new DeleteAction(),
+                new Exit()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()), is(nullValue()));
+    }
+
+    @Test
+    public void whenCreateItem() {
+        Input in = new StubInput(
+                new String[] {"0", "Item name", "1"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
+                new CreateAction(),
                 new Exit()
         };
-        new StartUI(out).init(in, tracker, actions);
-        assertThat(out.toString(), is(
-                "Menu." + System.lineSeparator()
-                        + "0.Exit" + System.lineSeparator()
-
-        ));
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName(), is("Item name"));
     }
 }
