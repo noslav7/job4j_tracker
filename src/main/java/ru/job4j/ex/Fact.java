96202f75323b1b3932fbf8d5d58fc98ede44befc
noslav7 <noslav7@gmail.com>
2021-07-03 21:03:12 +0500

    0.3. Тестирование исключений с junit [#219366]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -5,9 +5,9 @@ public class Fact {
 
     }
 
-    public int calc(int n) {
+    public static int calc(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("n must be zero or greater.");
+            throw new IllegalArgumentException("N could not be less then 0.");
         }
         int rsl = 1;
         for (int index = 1; index <= n; index++) {
