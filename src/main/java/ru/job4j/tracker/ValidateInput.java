0482ee8806dfe5c0a11a2a322b1ddae17e9d9155
noslav7 <noslav7@gmail.com>
2021-07-05 15:05:08 +0500

    1. Обеспечить бесперебойную работу приложения Tracker [#789 #174164]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,6 +1,6 @@
 package ru.job4j.tracker;
 
-public class ValidateInput extends ConsoleInput{
+public class  ValidateInput extends ConsoleInput{
 
     @Override
     public int askInt(String question) {
