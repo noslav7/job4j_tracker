0327f74949ad89f117c7213cdcf2fa1502f50f7f
noslav7 <noslav7@gmail.com>
2021-07-07 08:44:52 +0500

    2. Singleton [#94606]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,17 +1,29 @@
 package ru.job4j.tracker;
 
+import com.sun.jdi.connect.Connector;
+
 public class SingleTracker {
     private Tracker tracker = new Tracker();
+    private static Tracker instance = null;
 
-
+    public Tracker getInstance() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
+        return tracker;
+    }
 
     public Item add(Item item) {
         return tracker.add(item);
     }
 
-    public Item findById() {
-        return null;
+    public Item findById() { return tracker.findById(findById().getId());
     }
 
+    public Item[] findByName(String key) { return tracker.findByName(findById().getName()); }
+
+    public boolean replace(int id, Item item) { return tracker.replace(findById().getId(), item); }
+
+    public boolean delete(int id) { return tracker.delete(findById().getId()); }
 
 }
