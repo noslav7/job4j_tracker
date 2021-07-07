44fcb13da20508d42b6c22faf4643a4ac386676a
noslav7 <noslav7@gmail.com>
2021-07-07 17:08:57 +0500

    4. final [#334073 #175004]

--- src/main/java/ru/job4j/encapsulation/ooa/Airbus.java
+++ src/main/java/ru/job4j/encapsulation/ooa/Airbus.java
@@ -1,7 +1,7 @@
 package ru.job4j.encapsulation.ooa;
 
 public class Airbus extends Aircraft {
-    private final int COUNT_ENGINE = 2;
+    private static final int COUNT_ENGINE = 2;
     private String name;
 
     public Airbus(String name) {
