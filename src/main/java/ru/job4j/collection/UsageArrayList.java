f396507c132447a6347f181ca01572c25357112d
noslav7 <noslav7@gmail.com>
2021-07-09 19:09:16 +0500

    0. Коллекции, ArrayList, List, Обобщения [#10094 #175815]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class UsageArrayList {
     public static void main(String[] args) {
-        ArrayList<String> cells = new ArrayList<String>();
+        ArrayList<String> cells = new ArrayList<>();
         cells.add(0, "Petr");
         cells.add(1, "Ivan");
         cells.add(2, "Stepan");
