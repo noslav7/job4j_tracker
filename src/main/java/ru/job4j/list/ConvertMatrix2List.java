94e924406789389df7c568c7c00f85cf7a782279
noslav7 <noslav7@gmail.com>
2021-07-10 15:20:58 +0500

    ConvertMatrix2List.java

--- src/main/java/ru/job4j/list/ConvertMatrix2List.java
+++ src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -7,7 +7,7 @@ public class ConvertMatrix2List {
         List<Integer> list = new ArrayList<>();
         for (int[] row : array) {
             for (int cell : row) {
-                list.add(row[cell]);
+                list.add(cell);
             }
         }
         return list;
