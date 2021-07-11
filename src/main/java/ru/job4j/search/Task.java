eafc8c0eed5a507363fb7b8dfa85772ec9996374
noslav7 <noslav7@gmail.com>
2021-07-11 21:45:37 +0500

    1. Уникальные задачи. [#212653 #176287]

--- src/main/java/ru/job4j/search/Task.java
+++ src/main/java/ru/job4j/search/Task.java
@@ -1,19 +1,15 @@
 package ru.job4j.search;
 
 public class Task {
-    private String desc;
-    private int priority;
+    private String number;
+    private String description;
 
-    public Task(String desc, int priority) {
-        this.desc = desc;
-        this.priority = priority;
+    public Task(String number, String description) {
+        this.number = number;
+        this.description = description;
     }
 
-    public String getDesc() {
-        return desc;
-    }
-
-    public int getPriority() {
-        return priority;
+    public String getNumber() {
+        return number;
     }
 }
