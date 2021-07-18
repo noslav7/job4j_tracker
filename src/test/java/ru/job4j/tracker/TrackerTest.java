4f90455a9976a3d63c110049a23655451a4a50a3
noslav7 <noslav7@gmail.com>
2021-07-18 13:44:30 +0500

    TrackerTest.java

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
@@ -23,7 +23,7 @@ public class TrackerTest {
         Item second = new Item("Second");
         tracker.add(first);
         tracker.add(second);
-        Item result = tracker.findAll()[0];
+        Item result = tracker.findAll().get(0);
         assertThat(result.getName(), is(first.getName()));
     }
 
@@ -37,7 +37,7 @@ public class TrackerTest {
         tracker.add(new Item("First"));
         tracker.add(new Item("Second"));
         tracker.add(new Item("First"));
-        Item[] result = tracker.findByName(first.getName());
+        Item[] result = tracker.findByName(first.getName()).toArray(new Item[0]);
         assertThat(result.length, is(3));
     }
 
@@ -51,7 +51,7 @@ public class TrackerTest {
         tracker.add(new Item("First"));
         tracker.add(new Item("Second"));
         tracker.add(new Item("First"));
-        Item[] result = tracker.findByName(second.getName());
+        Item[] result = tracker.findByName(second.getName()).toArray(new Item[0]);
         assertThat(result[1].getName(), is(second.getName()));
     }
 
