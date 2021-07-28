90c6780772f10f4e8e12959dd626707e07f547e5
noslav7 <noslav7@gmail.com>
2021-07-28 20:01:36 +0500

    1.3. Встроенные функциональные интерфейсы. [#249209 #182842]

--- src/main/java/ru/job4j/function/SearchFolder.java
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -10,7 +10,7 @@ public class SearchFolder {
         List<Folder> folders = new ArrayList<>();
         pred = s -> s.getName().contains("bug");
         for (Folder f : list) {
-            if (pred.test(f)) {
+            if (pred.test(f))  {
                 folders.add(f);
             }
         }
