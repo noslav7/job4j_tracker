114259ff7f943f37769034cb4bf7fa312628a9b6
noslav7 <noslav7@gmail.com>
2021-07-29 09:45:37 +0500

    4. Зона видимости в лямбда-выражениях и исключения [#70609]

---
+++ src/main/java/ru/job4j/lambda/Diapason.java
@@ -0,0 +1,20 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class Diapason {
+
+    List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> results = new ArrayList<>();
+        Function<Double, Double> linear = s -> s;
+        results.add(linear.apply((double) start));
+        Function<Double, Double> square = s -> s * s;
+        results.add(square.apply((double) start * start));
+        Function<Double, Double> exponential = s -> (int) s ^ (int) s;
+        results.add(exponential.apply((double) (start ^ end)));
+    return results;
+    }
+    }
+
