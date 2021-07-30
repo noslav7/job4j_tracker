c1286951f9cf3ad4d973c78d2a17e9c25a90973b
noslav7 <noslav7@gmail.com>
2021-07-30 11:44:30 +0500

    Exercises Lambda

---
+++ src/main/java/ru/job4j/lambda/MethodReference.java
@@ -0,0 +1,25 @@
+package ru.job4j.lambda;
+
+public class MethodReference {
+
+    /*
+    public void applyByInstance() {
+        // your code here
+        consumer.accept("Hello");
+    }
+
+    public static void applyByClass() {
+        // your code here
+        consumer.accept("Hello");
+    }
+
+    private void consumerByInstance(String input) {
+        System.out.print("From instance: " + input);
+    }
+
+    private static void consumerByClass(String input) {
+        System.out.print("From class: " + input);
+    }
+
+     */
+}
