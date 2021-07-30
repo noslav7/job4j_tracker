e77896d3fa016496f780763230d1ecc21bdfa4da
noslav7 <noslav7@gmail.com>
2021-07-30 11:35:11 +0500

    5. Подсчет функции в диапазоне. [#24251 #183113]

--- src/main/java/ru/job4j/lambda/Diapason.java
+++ src/main/java/ru/job4j/lambda/Diapason.java
@@ -6,7 +6,7 @@ import java.util.function.Function;
 
 public class Diapason {
 
-    List<Double> diapason(int start, int end, Function<Double, Double> func) { //Как учитываются в решении результаты 3 необходимых функций?
+    static List<Double> diapason(int start, int end, Function<Double, Double> func) { //Как учитываются в решении результаты 3 необходимых функций?
         List<Double> results = new ArrayList<>();
         for (int i = start; i <= end; i++) {
             results.add(func.apply((double) i));
--- src/test/java/ru/job4j/lambda/DiapasonTest.java
+++ src/test/java/ru/job4j/lambda/DiapasonTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import ru.job4j.function.Folder;
 import ru.job4j.function.SearchFolder;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -14,13 +15,12 @@ import static org.junit.Assert.assertThat;
 public class DiapasonTest {
 
     @Test
-    public void whenDiapasonSmall() {
-        int start = 1;
-        int end = 3;
-        double expected = Math.pow(3, 1);
-        Function<Double, Double> exponential = f -> Math.pow(end, start);
-        double outcome = exponential.apply();
-        assertThat(outcome, is(expected));
+    public void whenSquareFunctionThenSquareResults() {
+        List<Double> result = Diapason.diapason(1, 4, x -> x * x);
+        List<Double> expected = Arrays.asList(1D, 4D, 9D, 16D);
+        assertThat(result, is(expected));
     }
+
+
 
 }
