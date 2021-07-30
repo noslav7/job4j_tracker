8a2742a4bde6ec7b58f2b78b2d59fb11dcbe9f84
noslav7 <noslav7@gmail.com>
2021-07-30 14:52:17 +0500

    5. Подсчет функции в диапазоне. [#24251 #183113]

--- src/test/java/ru/job4j/lambda/DiapasonTest.java
+++ src/test/java/ru/job4j/lambda/DiapasonTest.java
@@ -12,22 +12,22 @@ public class DiapasonTest {
 
     @Test
     public void whenLinearFunctionThenLinearResults() {
-        List<Double> result = Diapason.diapason(6, 8, x -> 5 * x);
-        List<Double> expected = Arrays.asList(30D, 35D, 40D);
+        List<Double> result = Diapason.diapason(6, 8, x -> 5 * x + 3);
+        List<Double> expected = Arrays.asList(33D, 38D, 43D);
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenSquareFunctionThenSquareResults() {
-        List<Double> result = Diapason.diapason(1, 4, x -> 2 * x * x);
-        List<Double> expected = Arrays.asList(2D, 8D, 18D, 32D);
+        List<Double> result = Diapason.diapason(1, 4, x -> 2 * x * x + 4);
+        List<Double> expected = Arrays.asList(6D, 12D, 22D, 36D);
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenExponentialFunctionThenExponentialResults() {
-        List<Double> result = Diapason.diapason(2, 6, x -> Math.pow(2, x));
-        List<Double> expected = Arrays.asList(4D, 8D, 16D, 32D, 64D);
+        List<Double> result = Diapason.diapason(2, 6, x -> Math.pow(2, x) + 5);
+        List<Double> expected = Arrays.asList(9D, 13D, 21D, 37D, 69D);
         assertThat(result, is(expected));
     }
 }
