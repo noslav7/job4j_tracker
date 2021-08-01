325da8bd670896e07875f3bb1e032dff923dfef4
noslav7 <noslav7@gmail.com>
2021-08-01 16:38:29 +0500

    1. Фильтрация учеников [#110058]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,23 +1,13 @@
 package ru.job4j.stream;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
-        public class School {
+public class School {
 
             public List<Student> collect(List<Student> students, Predicate<Student> predict) {
-
-                students.stream()
-                        .filter(score -> score.getScore() >= 70)
-                        .map (student -> student.getScore() + " " + student.getSurname());
-
-                students.stream()
-                        .filter(score -> score.getScore() >= 50 && score.getScore() < 70)
-                        .map (student -> student.getScore() + " " + student.getSurname());
-
-                students.stream()
-                        .filter(score -> score.getScore() < 50)
-                        .map (student -> student.getScore() + " " + student.getSurname());
-
-                return students;
+                return  students.stream()
+                        .filter(predict)
+                        .collect (Collectors.toList());
     }
 }
