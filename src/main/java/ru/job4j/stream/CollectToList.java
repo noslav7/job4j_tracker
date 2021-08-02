9a4793be832ae1c53bf8ff5646dbe0f0dbb086b3
noslav7 <noslav7@gmail.com>
2021-08-02 20:27:52 +0500

    Exersises Stream API

---
+++ src/main/java/ru/job4j/stream/CollectToList.java
@@ -0,0 +1,11 @@
+package ru.job4j.stream;
+
+public class CollectToList {
+    /*
+    public static List<Integer> collect(Stream<Integer> data) {
+        List<Integer> result = List.of(data).stream()
+                .collect(Collectors,toList()
+                        .Function.identity());
+        return result;
+    }*/
+}
---
+++ src/test/java/ru/job4j/stream/CollectToListTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.stream;
+
+public class CollectToListTest {
+/*
+    @Test
+    public void test() {
+        List<Integer> input = List.of(1, 2);
+        List<Integer> expect = List.of(1, 2);
+        assertEquals(expect, CollectToList.collect(input.stream()));
+    }
+*/
+}
