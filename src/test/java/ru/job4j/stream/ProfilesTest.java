61e2278054abae894cf904dd09bfdd32f7304152
noslav7 <noslav7@gmail.com>
2021-08-02 15:41:22 +0500

    2. Список адресов [#110062 #184383]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -5,22 +5,31 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class ProfilesTest {
-/*
+
     @Test
     public void whenCollectClassA() {
-        List<Profile> profiles = new ArrayList<Profile>() {
+        List<Profile> profiles = new ArrayList<>(
                 new Profile(new Address("Yekaterinburg", "Studencheskaya", 51, 413)),
-                new Profile(new Address("Yekaterinburg", "Studencheskaya", 51, 413)),
+                new Profile(new Address("Yekaterinburg", "Malysheva", 110, 185)),
                 new Profile(new Address("Yekaterinburg", "Soyuznaya", 6, 401)),
                 new Profile(new Address("Yekaterinburg", "8 marta", 172, 407)),
                 new Profile(new Address("Yekaterinburg", "Lenina", 51, 370))
-        };
-        List<Address> expected = Profiles.collect(profiles);
-        assertThat(profiles, is(expected));
-    }*/
+        );
+        List<Address> expected = profiles.stream().collect(List<Profile> profiles);
+        List<Address> addresses = new ArrayList<>();
+                addresses = List.of(
+                        new Address("Yekaterinburg", "Studencheskaya", 51, 413),
+                        new Address("Yekaterinburg", "Malysheva", 110, 185),
+                        new Address("Yekaterinburg", "Soyuznaya", 6, 401),
+                        new Address("Yekaterinburg", "8 marta", 172, 407),
+                        new Address("Yekaterinburg", "Lenina", 51, 370)
+                );
+        assertThat(addresses, is(expected));
+    }
 }
