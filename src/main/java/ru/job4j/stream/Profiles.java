d301fcaefec1d65c2faba60f97833f2a5441c9c7
noslav7 <noslav7@gmail.com>
2021-08-04 11:53:50 +0500

    Profiles.java

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -4,11 +4,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-
+/*
     public List<Address> collect(List<Address> addresses) {
         return addresses.stream()
                 .distinct()
                 .sorted(Comparator<Address>(String::getCity))
                 .collect(Collectors.toList());
-    }
+    }*/
 }
