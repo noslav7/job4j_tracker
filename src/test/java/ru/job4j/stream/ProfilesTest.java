b12349fa2fed2acebedd1ea1d74d27bbbaa5b2f3
noslav7 <noslav7@gmail.com>
2021-08-04 15:05:04 +0500

    3. Уникальность элементов и сортировка. [#110225 #185429]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -9,24 +9,22 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ProfilesTest {
-/*
+
     @Test
     public void whenFiveProfiles() {
         List<Profile> profiles = Arrays.asList(
                 new Profile(new Address("Yekaterinburg", "Studencheskaya", 51, 413)),
-                new Profile(new Address("Yekaterinburg", "Malysheva", 110, 185)),
-                new Profile(new Address("Yekaterinburg", "Soyuznaya", 6, 401)),
-                new Profile(new Address("Yekaterinburg", "8 marta", 172, 407)),
-                new Profile(new Address("Yekaterinburg", "Lenina", 51, 370))
+                new Profile(new Address("Yekaterinburg", "Studencheskaya", 51, 413)),
+                new Profile(new Address("Verkhnyaya Salda", "Lenina", 51, 370)),
+                new Profile(new Address("Moscow", "8 marta", 172, 407)),
+                new Profile(new Address("Verkhnyaya Salda", "Lenina", 51, 370))
         );
         List<Address> expected = new Profiles().collect(profiles);
         List<Address> addresses = List.of(
-                        new Address("Yekaterinburg", "Studencheskaya", 51, 413),
-                        new Address("Yekaterinburg", "Malysheva", 110, 185),
-                        new Address("Yekaterinburg", "Soyuznaya", 6, 401),
-                        new Address("Yekaterinburg", "8 marta", 172, 407),
-                        new Address("Yekaterinburg", "Lenina", 51, 370)
+                        new Address("Moscow", "8 marta", 172, 407),
+                        new Address("Verkhnyaya Salda", "Lenina", 51, 370),
+                        new Address("Yekaterinburg", "Studencheskaya", 51, 413)
                 );
-        assertThat(addresses, is(expected));
-    }*/
+        assertThat(expected, is(addresses));
+    }
 }
