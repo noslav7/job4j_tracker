237416fe003fbd88b86aa6fba48f89137051e15b
noslav7 <noslav7@gmail.com>
2021-08-05 11:54:03 +0500

    FlatIt.java

--- src/main/java/ru/job4j/stream/FlatIt.java
+++ src/main/java/ru/job4j/stream/FlatIt.java
@@ -1,10 +1,19 @@
 package ru.job4j.stream;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 public class FlatIt {
 /*
     public static List<Integer> flatten(Iterator<Iterator<Integer>> it) {
-        return Stream.of(it)
-                .flatMap((it1, it2) -> Stream.of(it1))
-                .collect(Collectors.toList());
+        return StreamSupport.stream(
+                Spliterators.spliteratorUnknownSize(it, Spliterator.ORDERED),
+                false);
     }*/
 }
