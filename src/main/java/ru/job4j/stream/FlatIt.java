d329eb6b4fc2fbe97882d24a3a8eaf4d483e78e4
noslav7 <noslav7@gmail.com>
2021-08-05 21:46:30 +0500

    Упражнения Stream API

--- src/main/java/ru/job4j/stream/FlatIt.java
+++ src/main/java/ru/job4j/stream/FlatIt.java
@@ -1,19 +1,10 @@
 package ru.job4j.stream;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Spliterator;
-import java.util.Spliterators;
-import java.util.function.UnaryOperator;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
-
 public class FlatIt {
 /*
     public static List<Integer> flatten(Iterator<Iterator<Integer>> it) {
-        return StreamSupport.stream(
-                Spliterators.spliteratorUnknownSize(it, Spliterator.ORDERED),
-                false);
+        return List.of(it).stream()
+                .flatMap(i -> i)
+                .collect(Collectors.toList());
     }*/
 }
