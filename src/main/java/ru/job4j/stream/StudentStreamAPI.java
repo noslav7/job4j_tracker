0b6e514fbdab78dfd6c55f393fe9294a98565e2e
noslav7 <noslav7@gmail.com>
2021-08-05 11:59:38 +0500

    for TravisCI

--- src/main/java/ru/job4j/stream/StudentStreamAPI.java
+++ src/main/java/ru/job4j/stream/StudentStreamAPI.java
@@ -41,8 +41,12 @@ public class StudentStreamAPI {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         StudentStreamAPI student = (StudentStreamAPI) o;
         return Objects.equals(account, student.account);
     }
