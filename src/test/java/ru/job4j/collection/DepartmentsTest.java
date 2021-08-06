bb01b61250811d3cf9d1235d0452ed2d5293a46f
noslav7 <noslav7@gmail.com>
2021-08-06 10:15:55 +0500

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -8,11 +8,11 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class DepartmentsTest {
-/*
+
     @Test
     public void whenMissed() {
         List<String> input = Arrays.asList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
@@ -20,9 +20,9 @@ public class DepartmentsTest {
     @Test
     public void whenNonChange() {
         List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
-*/
+
 }
--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -14,12 +14,12 @@ public class NotifyAccountTest {
 
     @Test
     public void sent() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001")
         );
         HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+                List.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
                 )
@@ -29,7 +29,7 @@ public class NotifyAccountTest {
 
     @Test
     public void  sentDuplicate() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("321", "Alexander Kovalenko", "eDer3432f"),
                 new Account("321", "Alexander Kovalenko", "eDer3432f")
         );
