8952a3851bc219cafdbd54c8d71a020bb825d880
noslav7 <noslav7@gmail.com>
2021-08-07 14:36:15 +0500

    for Travis CI

--- src/main/java/ru/job4j/stream/StudentLevel.java
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-
 public class StudentLevel {
     public static List<Student> levelOf(List<Student> students, int bound) {
         return students.stream()
