5b86a3677c8f8c5a9a0c40ee66dc47981fafda87
noslav7 <noslav7@gmail.com>
2021-08-07 09:53:11 +0500

    3. Optional в банковских переводах. [#214711 #186790]

--- src/test/java/ru/job4j/bank/BankServiceTest.java
+++ src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -1,7 +1,9 @@
 package ru.job4j.bank;
 
+import org.junit.Test;
+
 public class BankServiceTest {
-/*
+
     @Test
     public void addUser() {
         User user = new User("3434", "Petr Arsentev");
@@ -37,5 +39,5 @@ public class BankServiceTest {
         bank.addAccount(user.getPassport(), new Account("113", 50D));
         bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 150D);
         assertThat(bank.findByRequisite(user.getPassport(), "113").getBalance(), is(200D));
-    }*/
+    }
 }
