37fa9fc02ade6f807f654e7797827706b6187652
noslav7 <noslav7@gmail.com>
2021-08-25 12:58:54 +0500

    Exercises

---
+++ src/main/java/ru/job4j/map/MostUsedCharacter.java
@@ -0,0 +1,11 @@
+package ru.job4j.map;
+import java.util.*;
+
+public class MostUsedCharacter {
+    public static char getMaxCount(String str) {
+        Map<Character, Integer> mostUsedTree = new TreeMap<>();
+
+        char rsl = ' ';
+        return rsl;
+    }
+}
