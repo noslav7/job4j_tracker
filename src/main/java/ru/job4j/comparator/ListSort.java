3dd4e7782c6ac08f7f02ba3814b75e4f99def3e8
noslav7 <noslav7@gmail.com>
2021-11-09 08:42:24 +0500

    Exercises

--- src/main/java/ru/job4j/comparator/ListSort.java
+++ src/main/java/ru/job4j/comparator/ListSort.java
@@ -1,18 +1,12 @@
 package ru.job4j.comparator;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSort {
     public static List<String> sort(List<String> data) {
-        data.sort(Comparator.naturalOrder());
-        for (int i = 0; i < data.size() / 2; i++) {
-            if (data.get(i).equals("null")) {
-                String temp = data.get(i);
-                data.set(i, data.get(data.size() - i - 1));
-                data.set(data.size() - i - 1, temp);
-            }
-        }
+        data.sort(Comparator.nullsLast(Collections.reverseOrder()));
         return data;
     }
 }
--- src/test/java/ru/job4j/comparator/ListSortTest.java
+++ src/test/java/ru/job4j/comparator/ListSortTest.java
@@ -1,7 +1,13 @@
 package ru.job4j.comparator;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class ListSortTest {
-/*
+
     @Test
     public void test() {
         List<String> input = Arrays.asList(null, "a", "b", "c");
@@ -9,5 +15,4 @@ public class ListSortTest {
         List<String> expect = Arrays.asList("c", "b", "a", null);
         Assert.assertEquals(expect, result);
     }
-*/
 }
