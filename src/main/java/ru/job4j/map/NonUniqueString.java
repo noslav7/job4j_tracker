5b48730e723577903c0e46b1ae6dad2744d860e6
Valentin Rachkov <noslav7@gmail.com>
2021-11-15 18:22:40 +0500

    Exercises

--- src/main/java/ru/job4j/map/NonUniqueString.java
+++ src/main/java/ru/job4j/map/NonUniqueString.java
@@ -1,11 +1,28 @@
 package ru.job4j.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class NonUniqueString {
 
     public static Map<String, Boolean> checkData(String[] strings) {
-
-        return Map.of();
+        Map<String, Boolean> map = new HashMap<>();
+        for (int i = 0; i < strings.length; i++) {
+            int count = 0;
+            boolean value = false;
+            for (int j = 0; j < strings.length; j++) {
+                if (strings[i].equals(strings[j])) {
+                    count++;
+                }
+                    if (count >= 2) {
+                        value = true;
+                        map.put(strings[j], value);
+                    }
+                }
+            if (count == 1) {
+                map.put(strings[i], value);
+            }
+            }
+        return map;
     }
 }
