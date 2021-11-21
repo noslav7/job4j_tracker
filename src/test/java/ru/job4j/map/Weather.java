75cda0eae919ef650059a8c37ee49372b3859e27
Valentin Rachkov <noslav7@gmail.com>
2021-11-21 14:09:46 +0500

    Exercises

---
+++ src/test/java/ru/job4j/map/Weather.java
@@ -0,0 +1,64 @@
+package ru.job4j.map;
+
+import java.util.*;
+
+public class Weather {
+    public static List<Info> editData(List<Info> list) {
+        List<Info> rsl = new ArrayList<>();
+        for (Info i: list) {
+            int rain = 0;
+            for (Info j : list) {
+                if (i.city.equals(j.city)) {
+                    rain += j.rainfall;
+                }
+            }
+            Info oneInfo = new Info(i.city, rain);
+            if (!rsl.contains(oneInfo)) {
+                rsl.add(oneInfo);
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public static class Info {
+        private String city;
+
+        private int rainfall;
+
+        public Info(String city, int rainfall) {
+            this.city = city;
+            this.rainfall = rainfall;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public int getRainfall() {
+            return rainfall;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Info info = (Info) o;
+            return rainfall == info.rainfall &&
+                    Objects.equals(city, info.city);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(city, rainfall);
+        }
+    }
+}
