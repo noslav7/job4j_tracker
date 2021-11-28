973be96e953d241327090af32a046913176ae628
Valentin Rachkov <noslav7@gmail.com>
2021-11-28 20:18:22 +0500

    2. Отсортировать департаменты [#34352 #233605]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -6,6 +6,13 @@ public class DepDescComp implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-        return o1.compareTo(o2);
+        String[] firstArray = o1.split("/");
+        String[] secondArray = o2.split("/");
+        int rsl = secondArray[0].compareTo(firstArray[0]);
+        if (rsl == 0) {
+            return o1.compareTo(o2);
+        } else {
+            return rsl;
+        }
     }
 }
