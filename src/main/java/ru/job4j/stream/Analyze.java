fa5758fe4f9bbd3267994a509aedcf8c1fe96818
noslav7 <noslav7@gmail.com>
2021-12-29 11:15:23 +0500

    Streams

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -26,7 +26,8 @@ public class Analyze {
         Map<String, Double> map = stream
                 .flatMap(pupil -> pupil.getSubjects().stream())
                 .collect(Collectors.groupingBy(Subject::getName,
-                        Collectors.averagingDouble(subj -> Double.parseDouble(subj.getScore() + ""))));
+                        Collectors.averagingDouble(subj -> Double
+                                .parseDouble(subj.getScore() + ""))));
         return map.entrySet()
                 .stream()
                 .map(x -> new Tuple(x.getKey(), x.getValue()))
