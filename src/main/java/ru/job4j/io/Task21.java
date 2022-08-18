484794b00633f7157cf1a3514fac125a530fc8f3
noslav7 <noslav7@gmail.com>
2022-08-18 19:10:16 +0500

    Task21.jav`a

---
+++ src/main/java/ru/job4j/io/Task21.java
@@ -0,0 +1,14 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+
+public class Task21 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int columnCount = in.nextInt();
+        String star = "*";
+        String space = " ";
+
+
+    }
+}
