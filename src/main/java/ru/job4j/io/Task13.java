fdc5ae1472333cc833dbe17ce53e917a3ef671e4
noslav7 <noslav7@gmail.com>
2022-09-07 09:24:25 +0500

    exercises

--- src/main/java/ru/job4j/io/Task13.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.io;
-
-public class Task13 {
-    public static void main(String[] args) {
-        System.out.print("H-e-l-l-o-,-f-r-i-e-n-d!");
-    }
-}
--- src/main/java/ru/job4j/io/Task18.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.io;
-
-public class Task18 {
-    public static void main(String[] args) {
-        int x = 9;
-        System.out.println("Значение x равно " + x);
-        System.out.print("Значение x^2 равно " + x * x);
-    }
-}
