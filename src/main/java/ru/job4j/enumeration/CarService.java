131c84567e294951f1aa80d9153d0f9d36f63857
noslav7 <noslav7@gmail.com>
2023-01-19 11:58:08 +0500

    Enum

---
+++ src/main/java/ru/job4j/enumeration/CarService.java
@@ -0,0 +1,9 @@
+package ru.job4j.enumeration;
+
+public class CarService {
+    public static void main(String[] args) {
+        Order order = new Order(1, "Mercedes-Benz GLS", Status.IN_WORK);
+        System.out.println("Заказ-наряд №" + order.getNumber() + " на автомобиль " + order.getCar()
+                + ", статус ремонта: " + order.getStatus().getInfo());
+    }
+}
