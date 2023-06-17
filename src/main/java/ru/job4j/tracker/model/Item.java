a2b4d54a78add801a09890fa24f15bdcfcb23e98
noslav7 <noslav7@gmail.com>
2023-06-17 11:04:13 +0500

    2. @Setter, @Getter и другие [#504889 # [#504889]]

--- src/main/java/ru/job4j/tracker/model/Item.java
+++ src/main/java/ru/job4j/tracker/model/Item.java
@@ -7,13 +7,10 @@ import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 @ToString
-@RequiredArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Item {
 
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-    @NonNull
     @Getter
     @Setter
     private int id;
@@ -37,5 +34,22 @@ public class Item {
         this.id = id;
         this.name = name;
         this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return id == item.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
