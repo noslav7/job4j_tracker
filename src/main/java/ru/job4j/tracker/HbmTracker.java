737ce1ef054d07026691d4b2bf5eabed47bde2cd
noslav7 <noslav7@gmail.com>
2023-06-30 10:29:06 +0500

    3. HbmTracker [#287705 # [#287705]]

--- src/main/java/ru/job4j/tracker/HbmTracker.java
+++ src/main/java/ru/job4j/tracker/HbmTracker.java
@@ -1,11 +1,14 @@
 package ru.job4j.tracker;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class HbmTracker implements Store, AutoCloseable {
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
@@ -15,32 +18,102 @@ public class HbmTracker implements Store, AutoCloseable {
 
     @Override
     public Item add(Item item) {
-        return null;
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.save(item);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        session.close();
+        return item;
     }
 
     @Override
     public boolean replace(Integer id, Item item) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "UPDATE Item SET name = :fName WHERE id = :fId", Item.class)
+                    .setParameter("fName", item.getName())
+                    .setParameter("fId", id);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
         return false;
     }
 
     @Override
     public boolean delete(Integer id) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "DELETE Item i WHERE i.id = :fId", Item.class)
+                    .setParameter("fId", id);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
         return false;
     }
 
     @Override
     public List<Item> findAll() {
-        return null;
+        Session session = sf.openSession();
+        List<Item> result = Collections.emptyList();
+        try {
+            session.beginTransaction();
+            result = session.createQuery(
+                            "from Item", Item.class)
+                    .list();
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return result;
     }
 
     @Override
     public List<Item> findByName(String key) {
-        return null;
+        Session session = sf.openSession();
+        List<Item> result = Collections.emptyList();
+        try {
+            session.beginTransaction();
+            result = session.createQuery(
+                            "from Item i where i.name = :name", Item.class)
+                    .setParameter("name", key)
+                    .list();
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return result;
     }
 
     @Override
     public Item findById(Integer id) {
-        return null;
+        Session session = sf.openSession();
+        Item result = null;
+        try {
+            session.beginTransaction();
+            result = session.createQuery(
+                            "from Item i where i.id = :id", Item.class)
+                    .setParameter("id", id)
+                    .uniqueResult();
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return result;
     }
 
     @Override
