4d329fed5a9a510b0cc813d987a4f36243ed169b
noslav7 <noslav7@gmail.com>
2023-06-30 10:44:42 +0500

    3. HbmTracker [#287705 # [#287705]]

--- src/main/java/ru/job4j/tracker/Store.java
+++ src/main/java/ru/job4j/tracker/Store.java
@@ -4,9 +4,14 @@ import java.util.List;
 
 public interface Store {
     Item add(Item item);
+
     boolean replace(Integer id, Item item);
+
     boolean delete(Integer id);
+
     List<Item> findAll();
+
     List<Item> findByName(String key);
+
     Item findById(Integer id);
 }
